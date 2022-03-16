9bc40524ca103fd92a8ec9054bca43d0a3925ee4
范恩琪 <88081246+angelawirantono@users.noreply.github.com>
2022-03-16 16:42:17 +0800

    Add decrypt

---
+++ HW1/decrypt_vernam.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<sstream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	string input, key;
+	string inputFile = argv[4];
+	string outputFile = "output.txt";
+
+	ifstream inputF;
+	inputF.open(inputFile);
+	if (inputF.fail()) {
+		cout << "Read File Fail\n"; exit(1);
+	}
+	string line = "";
+	while (getline(inputF, line)) {
+		stringstream file(line);
+		file >> input;
+	}
+	//cout << "Enter text: ";
+	//getline(cin, input);
+	//cout << "Enter key: ";
+	//cin >> key;
+	key = argv[6];
+	int j = 0;
+	int mod = key.size();
+	for (int i = key.size(); i < input.size(); i++) {
+		key += key[j % mod]; j++;
+	}
+	string answer;
+	for (int i = 0; i < input.size(); i++) {
+		answer += (input[i] - key[i] + 26) % 26 + 'A';
+	}
+	//cout << "Decrypt text: ";
+	ofstream output;
+	output.open(outputFile);
+	output << answer;
+	output.close();
+	return 0;
+}
