972cef0c6ca15a465e3f5fdee9c039f9005f8384
羅翡幸 <93699416+feliciacia@users.noreply.github.com>
2022-05-17 14:02:10 +0800

    Update RSA.py

--- HW4/RSA.py
+++ HW4/RSA.py
@@ -30,11 +30,12 @@ def gcd(a,b):
 
 
 #to find not only the gcd, but also Bezout's Identity (sa + bt = gcd(a,b))
-def extend_gcd(a,b):
+#extend euclidean gcd
+def euclidean(a,b):
     if b == 0:
         return 1, 0, a
     else:
-        s, t, gcd = extend_gcd(b, a%b)
+        s, t, gcd = euclidean(b, a%b)
         s, t = t, (s - (a//b) * t)
         return s, t, gcd
 
@@ -43,53 +44,53 @@ def extend_gcd(a,b):
 
 
 #to find the inverse of e
-def invert(e,phi):
-    x, y, gcd = extend_gcd(e, phi)
-    return x%phi
+def inverse(e,phi):
+    a, b, gcd = euclidean(e, phi)
+    return a%phi
 
 
 # In[5]:
 
 
-#The main function to do the RSA and also to be used in the Miller Rabin Test. Similar to pow(x,h,n) in python
-def square_and_multiply(x, h, n):
-    y = 1
-    h = bin(h)[2:]
-    for i in range(len(h)):
-        y = (y ** 2) % n
-        if h[i] == '1':
-            y = (y * x) % n
-    return y
+#The main function to do the RSA and also to be used in the Miller Rabin Test. Similar to pow(base,power,n) in python
+def square_and_multiply(base, power, n):
+    x = 1
+    power = bin(power)[2:]
+    for i in range(len(power)):
+        x = (x ** 2) % n
+        if power[i] == '1':
+            x = (x * base) % n
+    return x
 
 
 # In[6]:
 
 
 #The Miller Rabin Test to check whether the given number is prime or not
-def miller_rabin_test(n, repeat = 40):
+def miller_rabin_test(n, check = 50):
     #if can be divided by 2, it is not prime, used to accelerate the search of prime
     if n%2 == 0:
         return False
     
-    #to write n-1 in the form (2^r)*s
-    r, s = 0, n-1
-    while s%2 == 0:
-        r += 1
-        s //= 2
+    #to write n-1 in the form (2^power)*multiplier
+    power, multiplier = 0, n-1
+    while multiplier%2 == 0:
+        power += 1
+        multiplier //= 2
     
-    #repeat for reassurance
-    for i in range(repeat):
+    #check for reassurance
+    for i in range(check):
         #pick a random number
         a = random.randrange(2,n-1)
         if a%2==0:
             a = random.randrange(2,n-1)
         #let x = a^s mod n
-        x = square_and_multiply(a, s, n)
+        x = square_and_multiply(a, multiplier, n)
         
         #if it is 1 or -1, then pick another number, else
         if x != 1 and x!= n-1:
             #loop x = x^2 % n. If x == n-1, return to the first loop
-            for j in range(r-1):
+            for j in range(power-1):
                 if x == n-1:
                     break
                 x = (x**2)%n
@@ -104,29 +105,29 @@ def miller_rabin_test(n, repeat = 40):
 # In[7]:
 
 
-#for production of p and q (1024 bits probable prime)
-def produce_big_prime(bits = 1024):
-    b_prime = random.getrandbits(bits)
+#for production large prime of p and q (1024 bits probable prime)
+def generate_primenum(bits = 1024):
+    primenum = random.getrandbits(bits)
     #miller_rabin_test works for values above 3. If the random number is not prime, then find new values
-    while miller_rabin_test(b_prime) == False or b_prime < 4:
-        b_prime = random.getrandbits(bits)
-    return b_prime
+    while miller_rabin_test(primenum) == False or primenum < 4:
+        primenum = random.getrandbits(bits)
+    return primenum
 
 
 # In[8]:
 
 
 #production of p, q, N, phi, e, d
-def produce_key(bits = 1024):
-    p = produce_big_prime()
-    q = produce_big_prime()
+def generate_key(bits = 1024):
+    p = generate_primenum()
+    q = generate_primenum()
     phi = (p-1) * (q-1)
     N = p * q
     
     e = random.randrange(phi)
     while gcd(e,phi) != 1:
         e = random.randrange(phi)
-    d = invert(e, phi)
+    d = inverse(e, phi)
     
     return p, q, N, phi, e, d
 
@@ -165,16 +166,16 @@ def decrypt(ciphertext, N, d):
 
 
 #decipher using chinese remainder theorem
-def decrypt_with_CRT(ciphertext, p, q, d):
-    dp = d % (p-1)
-    dq = d % (q-1)
-    q_inverse = invert(q, p)
+def CRT_decrypt(ciphertext, p, q, d):
+    decipher_p = d % (p-1)
+    decipher_q = d % (q-1)
+    q_inverse = inverse(q, p)
     
     cipher_bytes = int.from_bytes(base64.b64decode(ciphertext.encode('ascii')),"big")
-    m1 = square_and_multiply(cipher_bytes, dp, p)
-    m2 = square_and_multiply(cipher_bytes, dq, q)
-    h = (q_inverse* (m1 - m2)) % p
-    cipher_int = m2 + h * q
+    squaremultiply1 = square_and_multiply(cipher_bytes, decipher_p, p)
+    squaremultiply2 = square_and_multiply(cipher_bytes, decipher_q, q)
+    temp = (q_inverse* (squaremultiply1 - squaremultiply2)) % p
+    cipher_int = squaremultiply2 + temp * q
     ans = ""
     while cipher_int%256!=0:
         ans = str(chr(cipher_int%256)) + ans
@@ -200,5 +201,4 @@ if __name__ == "__main__":
     if sys.argv[1] == "-d":
         print(decrypt(sys.argv[2], int(sys.argv[3]), int(sys.argv[4])))
     if sys.argv[1] == '-CRT':
-        print(decrypt_with_CRT(sys.argv[2],int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5])))
-
+        print(CRT_decrypt(sys.argv[2],int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5])))
